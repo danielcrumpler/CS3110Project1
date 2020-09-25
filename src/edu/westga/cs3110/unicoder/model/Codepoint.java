3260f0c6da924904e97d992c626750fd09b7fc0f
Daniel Crumpler <dancrumpy@gmail.com>
2020-09-25 11:47:50 -0400

    Commit #4: UTF-16 Functionality and Tests.

--- src/edu/westga/cs3110/unicoder/model/Codepoint.java
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -1,8 +1,14 @@
 package edu.westga.cs3110.unicoder.model;
 
+/**
+ * Stores Codepoint value and converts to different UTF encodings.
+ * 
+ * @author Daniel Crumpler
+ * @version Fall 2020
+ */
 public class Codepoint {
 	
-	private String hexadecimal;
+	private String codepoint;
 	
 	/**
 	 * Creates a new Codepoint.
@@ -10,50 +16,89 @@ public class Codepoint {
 	 * @precondition 
 	 * @postcondition getHexadecimal() == hexadecimal
 	 * 
-	 * @param hexadecimal
+	 * @param codepoint the input codepoint string.
 	 */
-	public Codepoint(String hexadecimal) {
-		this.setHexadecimal(hexadecimal);
+	public Codepoint(String codepoint) {
+		if (!codepoint.matches("0*([0-7]{1}[0-9A-F]{7})|([0-9A-F]{1,7})")) {
+			throw new IllegalArgumentException("hexadecimal needs to match.");
+		}
+		this.setHexadecimal(codepoint);
 	}
 	
 	/**
+	 * Gets the Codepoint String
 	 * 
-	 * @return
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return the codepoint
 	 */
-	public String getHexadecimal() {
-		return hexadecimal;
+	public String getCodepoint() {
+		return this.codepoint;
 	}
 	
 	/**
+	 * Sets the value of the current codepoint to a new codepoint value
 	 * 
-	 * @param hexadecimal
+	 * @precondition
+	 * @postcondition
+	 * 
+	 * @param codepoint the input codepoint string.
 	 */
-	public void setHexadecimal(String hexadecimal) {
-		this.hexadecimal = hexadecimal;
+	public void setHexadecimal(String codepoint) {
+		this.codepoint = codepoint;
 	}
 
 	/**
+	 * Converts the Codepoint to UTF-32 Encoding.
 	 * 
-	 * @return
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return Encoded Hexadecimal String as UTF-32
 	 */
 	public String toUTF32() {
-		int parsed = Integer.parseUnsignedInt(this.hexadecimal, 16);
+		int parsed = Integer.parseUnsignedInt(this.codepoint, 16);
 		return String.format("%08X", parsed);
 	}
 	
 	/**
+	 * Converts the Codepoint to UTF-16 Encoding.
 	 * 
-	 * @return
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return Encoded Hexadecimal String as UTF-16
 	 */
 	public String toUTF16() {
-		return null;
+		int parsed = Integer.parseUnsignedInt(this.codepoint, 16);
+		if (parsed < 0x10000) {
+			return this.isTwoBytesOfUTF16(parsed);
+		} else {
+			parsed -= 0x10000;
+			int highSurrogate = 0xD800 + (parsed >> 10);
+			int lowSurrogate = 0xDC00 + (parsed & 0b00000000001111111111);
+			return String.format("%04X", highSurrogate) + String.format("%04X", lowSurrogate);
+		}
+	}
+
+	private String isTwoBytesOfUTF16(int parsed) {
+		if (parsed > 0xD7FF && parsed < 0xE000) {
+			return null;
+		} else {
+			return String.format("%04X", parsed);
+		}
 	}
 	
 	/**
+	 * Converts the Codepoint to UTF-8 Encoding.
 	 * 
-	 * @return
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return Encoded Hexadecimal String as UTF-8
 	 */
 	public String toUTF8() {
-		return null;
+		int parsed = Integer.parseUnsignedInt(this.codepoint, 16);
 	}
 }
--- src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestCodepointToUTF16.java
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestCodepointToUTF16.java
@@ -1,14 +1,57 @@
 package edu.westga.cs3110.unicoder.tests.model.codepoint;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3110.unicoder.model.Codepoint;
 
 class TestCodepointToUTF16 {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testTwoByteInZoneOne() {
+		Codepoint codepoint = new Codepoint("ABCD");
+		assertEquals("ABCD", codepoint.toUTF16());
+	}
+	@Test
+	void testTwoByteZoneOneMin() {
+		Codepoint codepoint = new Codepoint("0000");
+		assertEquals("0000", codepoint.toUTF16());
+	}
+	@Test
+	void testTwoByteZoneOneMax() {
+		Codepoint codepoint = new Codepoint("D7FF");
+		assertEquals("D7FF", codepoint.toUTF16());
+	}
+	
+	@Test
+	void testTwoByteInZoneTwo() {
+		Codepoint codepoint = new Codepoint("E00F");
+		assertEquals("E00F", codepoint.toUTF16());
+	}
+	
+	@Test
+	void testTwoByteZoneTwoMin() {
+		Codepoint codepoint = new Codepoint("E000");
+		assertEquals("E000", codepoint.toUTF16());
+	}
+	
+	@Test
+	void testTwoByteZoneTwoMax() {
+		Codepoint codepoint = new Codepoint("FFFF");
+		assertEquals("FFFF", codepoint.toUTF16());
+	}
+	
+	@Test
+	void testFourByte() {
+		Codepoint codepoint = new Codepoint("0183A5");
+		assertEquals("D820DFA5", codepoint.toUTF16());
+	}
+	
+	@Test
+	void testGapByte() {
+		Codepoint codepoint = new Codepoint("D8FF");
+		assertEquals(null, codepoint.toUTF16());
 	}
 
 }

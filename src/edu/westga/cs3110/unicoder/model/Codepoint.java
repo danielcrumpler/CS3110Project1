b62668055824796a60d2db6034a58f8f0eba6b44
Daniel Crumpler <dancrumpy@gmail.com>
2020-09-25 08:55:31 -0400

    Commit #2: More Project Layout

--- src/edu/westga/cs3110/unicoder/model/Codepoint.java
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -1,5 +1,58 @@
 package edu.westga.cs3110.unicoder.model;
 
 public class Codepoint {
+	
+	private String hexadecimal;
+	
+	/**
+	 * Creates a new Codepoint.
+	 * 
+	 * @precondition 
+	 * @postcondition getHexadecimal() == hexadecimal
+	 * 
+	 * @param hexadecimal
+	 */
+	public Codepoint(String hexadecimal) {
+		this.setHexadecimal(hexadecimal);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getHexadecimal() {
+		return hexadecimal;
+	}
+	
+	/**
+	 * 
+	 * @param hexadecimal
+	 */
+	public void setHexadecimal(String hexadecimal) {
+		this.hexadecimal = hexadecimal;
+	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	public String toUTF32() {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String toUTF16() {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String toUTF8() {
+		return null;
+	}
 }
